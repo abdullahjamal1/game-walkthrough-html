5725168b9f2b7b781e0c266ebaf363203c56ddd8
aj941ga <45927383+aj941ga@users.noreply.github.com>
2019-12-31 12:44:43 +0300

    Update welcome_get.php

--- welcome_get.php
+++ welcome_get.php
@@ -1,8 +1,8 @@
 <html>
 <body>
 
-Welcome back <?php echo $_GET["name"]; ?> commander <br>
-Your email address is: <?php echo $_GET["email"]; ?>
+Welcome back <?php echo $_POST["name"]; ?> commander <br>
+Your email address is: <?php echo $_POST["email"]; ?>
 
 </body>
 </html>
